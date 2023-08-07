29e6ca527891d92e74733fb522b0a99901c750a0
Karoline Cruz <karollcruz@hotmail.com>
2023-08-07 10:25:22 +0100

    Lessons to recap knowledge about CSS

---
+++ javascript-course/calculator/05-with-separate-files.html/script.js
@@ -0,0 +1,17 @@
+let calculation = localStorage.getItem('calculation') || '';
+
+      calculatorDisplay();
+
+      function updateCalculation(value) {
+        calculation += value;
+        calculatorDisplay(calculation);
+        saveCalculation();
+      }
+      
+      function saveCalculation() {
+        localStorage.setItem('calculation', calculation);
+      }
+
+      function calculatorDisplay() {
+        document.querySelector('.js-calculator-display').innerHTML = calculation; 
+      }
