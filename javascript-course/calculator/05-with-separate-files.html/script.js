1efb41453adf1b61cddc2a99f189eb2ac4d0978f
Karoline Cruz <karollcruz@hotmail.com>
2023-08-08 11:43:19 +0100

    Lessons to recap knowledge about JavaScript and CSS

--- javascript-course/calculator/05-with-separate-files.html/script.js
+++ javascript-course/calculator/05-with-separate-files.html/script.js
@@ -1,17 +1,17 @@
 let calculation = localStorage.getItem('calculation') || '';
 
-      calculatorDisplay();
+calculatorDisplay();
 
-      function updateCalculation(value) {
-        calculation += value;
-        calculatorDisplay(calculation);
-        saveCalculation();
-      }
-      
-      function saveCalculation() {
-        localStorage.setItem('calculation', calculation);
-      }
+function updateCalculation(value) {
+  calculation += value;
+  calculatorDisplay(calculation);
+  saveCalculation();
+}
 
-      function calculatorDisplay() {
-        document.querySelector('.js-calculator-display').innerHTML = calculation; 
-      }
+function saveCalculation() {
+  localStorage.setItem('calculation', calculation);
+}
+
+function calculatorDisplay() {
+  document.querySelector('.js-calculator-display').innerHTML = calculation; 
+}
