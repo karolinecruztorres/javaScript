058a0bcfe256ec12f4c84a534d7f16a53d3e8c32
Karoline Cruz <karollcruz@hotmail.com>
2023-10-02 08:36:36 +0100

    A Hangman Game. Still working on the code. Making the code along with the video 'Build A Hangman Game in HTML CSS and JavaScript | Hangman Game in HTML CSS and JavaScript'. Link: https://www.youtube.com/watch?v=hSSdc8vKP1I&t=623s.

---
+++ hangman/scripts/word-list.js
@@ -0,0 +1,82 @@
+const wordList = [
+  { 
+    word: "dance", 
+    hint: "A rhythmic movement of the body often performed to music" 
+  },
+  { 
+    word: "elephant", 
+    hint: "A large mammal with tusks and a trunk" 
+  },
+  { 
+    word: "blue", 
+    hint: "A color often associated with the sky and the ocean" 
+  },
+  { 
+    word: "guitar", 
+    hint: "A musical instrument with strings" 
+  },
+  { 
+    word: "apple", 
+    hint: "A fruit that is typically red or green" 
+  },
+  { 
+    word: "butterfly", 
+    hint: "An insect with colorful wings that undergo metamorphosis" 
+  },
+  { 
+    word: "book", 
+    hint: "A set of written or printed pages bound together" 
+  },
+  { 
+    word: "sunflower", 
+    hint: "A tall plant with a large, bright yellow flower head" 
+  },
+  { 
+    word: "mountain", 
+    hint: "A large landform that rises prominently above its surroundings" 
+  },
+  { 
+    word: "pizza", 
+    hint: "A popular Italian dish consisting of a flat, round base of dough topped with tomato sauce and cheese" 
+  },
+  { 
+    word: "piano", 
+    hint: "A large musical instrument with keys that produce sound when pressed" 
+  },
+  { 
+    word: "dolphin", 
+    hint: "A marine mammal known for its playful behavior and intelligence" 
+  },
+  { 
+    word: "red", 
+    hint: "A color on the visible spectrum with the longest wavelength" 
+  },
+  { 
+    word: "camera", 
+    hint: "A device used to capture and record still images or moving pictures" 
+  },
+  { 
+    word: "flower", 
+    hint: "The reproductive structure found in flowering plants" 
+  },
+  { 
+    word: "bird", 
+    hint: "A warm-blooded vertebrate with feathers, beak, and wings" 
+  },
+  { 
+    word: "computer", 
+    hint: "An electronic device that processes data and performs tasks according to a set of instructions" 
+  },
+  { 
+    word: "coffee", 
+    hint: "A popular beverage" 
+  },
+  { 
+    word: "key", 
+    hint: "A small metal instrument used for opening locks or operating a mechanism" 
+  },
+  { 
+    word: "moon", 
+    hint: "The natural satellite of the Earth" 
+  },
+];
