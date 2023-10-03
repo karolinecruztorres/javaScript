a3e547304446dad4b83b5cbbd03285c0efdc62de
Karoline Cruz <karollcruz@hotmail.com>
2023-10-03 17:45:32 +0100

    A code copy of the video 'Build A Weather App in HTML CSS & JavaScript | Weather App in JavaScript'. Link: https://www.youtube.com/watch?v=c1r-NqYkFPc&list=PLpwngcHZlPafPXPPPVTsOugzKfIJjB7Qm&index=9.

--- code-copies/weatherApp/script.js
+++ code-copies/weatherApp/script.js
@@ -1,7 +1,12 @@
 const wrapper = document.querySelector(".wrapper"),
 inputPart = wrapper.querySelector(".input-part"),
 infoTxt = inputPart.querySelector(".info-txt"),
-inputField = inputPart.querySelector("input");
+inputField = inputPart.querySelector("input"),
+locationBtn = inputPart.querySelector("button"),
+weatherIcon = wrapper.querySelector(".weather-part img"),
+arrowBack = wrapper.querySelector("header em");
+
+let api;
 
 inputField.addEventListener("keyup", e => {
   if(e.key == "Enter" && inputField.value != ""){
@@ -9,7 +14,71 @@ inputField.addEventListener("keyup", e => {
   }
 });
 
-function requestApi(city) {
-  let api = `https://api.openweathermap.org/data/2.5/weather?q=$
-  {city}&appid={005522df3133979a3460cf46f44cdbb9}`;
+locationBtn.addEventListener("click", ()=>{
+  if(navigator.geolocation){
+    navigator.geolocation.getCurrentPosition(onSuccess, onError);
+  }else{
+    alert("Your browser not support geolocation api");
+  }
+});
+
+function onSuccess(position){
+  const {latitude, longitude} = position.coords;
+  api = `http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&units=metric&appid=005522df3133979a3460cf46f44cdbb9`;
+  fetchData();
 }
+
+function onError(error){
+  infoTxt.innerText = error.message;
+  infoTxt.classList.add("error");
+}
+
+function requestApi(city) {
+  api = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=005522df3133979a3460cf46f44cdbb9`;
+  fetchData();
+}
+
+function fetchData() {
+  infoTxt.innerText = "Getting weather details...";
+  infoTxt.classList.add("pending");
+  fetch(api).then(response => response.json()).then(result => weatherDetails(result));
+}
+
+function weatherDetails(info) {
+  infoTxt.classList.replace("pending", "error");
+  if(info.cod == "404") {
+    infoTxt.innerText = `${inputField.value} isn't a valid city name`;
+  } else {
+    const city = info.name;
+    const country = info.sys.country;
+    const {description, id} = info.weather[0];
+    const {feels_like, humidity, temp} = info.main;
+
+    if(id == 800){
+      weatherIcon.src = "icons/clear.svg";
+    }else if (id >= 801 && id <= 804) {
+      weatherIcon.src = "icons/cloud.svg";
+    }else if (id >= 701 && id <= 781) {
+      weatherIcon.src = "icons/haze.svg";
+    }else if (id >= 300 && id <= 531) {
+      weatherIcon.src = "icons/rain.svg";
+    }else if (id >= 600 && id <= 622) {
+      weatherIcon.src = "icons/snow.svg";
+    }else if (id >= 200 && id <= 232) {
+      weatherIcon.src = "icons/storm.svg";
+    }  
+
+    wrapper.querySelector(".temp .numb").innerText = Math.floor(temp);
+    wrapper.querySelector(".weather").innerText = description;
+    wrapper.querySelector(".location span").innerText = `${city}, ${country}`;
+    wrapper.querySelector(".temp .numb-2").innerText = Math.floor(feels_like);
+    wrapper.querySelector(".humidity span").innerText = `${humidity}%`;
+    inputField.value = "";
+    infoTxt.classList.remove("pending", "error");
+    wrapper.classList.add("active");
+  }
+}
+
+arrowBack.addEventListener("click", ()=>{
+  wrapper.classList.remove("active");
+})
